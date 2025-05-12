bcd69791a0c054bd158da967a3573fa5b25d35a3
Elöd Dobos <elod.dobos@gmail.com>
2025-05-12 13:55:36 +0200

    Initial Commit

---
+++ dogApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dogApp
+//
+//  Created by eloddobos on 2025-05-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
